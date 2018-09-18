51972f3c6104e1242c5cfbdaafe2376ccf48ff63
Kcr19 <22756637+Kcr19@users.noreply.github.com>
2018-09-18 15:52:53 -0400

    Update mender_server_install.sh

--- tutorials/mender_gcp_ota_demo/server/mender_server_install.sh
+++ tutorials/mender_gcp_ota_demo/server/mender_server_install.sh
@@ -18,7 +18,7 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -a -G docker $USER
-git clone -b 1.5.0 https://github.com/mendersoftware/integration mender-server
+git clone -b 1.6.0 https://github.com/mendersoftware/integration mender-server
 cd mender-server
 git checkout -b my-production-setup
 cp -a template production
